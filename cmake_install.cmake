ace7bfb288960ccaa26c14ebf149642b3976653a
Twenmod <TwenGames@gmail.com>
2024-04-11 16:12:14 +0200

    changed cmake and changed gitignore

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/TwenG/source/repos/Twenmod/Portfoliogame
+# Install script for directory: C:/Users/TwenG/source/repos/Portfoliogame
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/TwenG/source/repos/Twenmod/Portfoliogame/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/TwenG/source/repos/Portfoliogame/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
